6be3acc7a04fa05b0d83c14a975102f48bf5ae41
Mark <mark@remarkablemark.org>
2016-04-17 20:06:18 -0400

    Tidy up codebase; use 4 spaces instead of 2

--- app.js
+++ app.js
@@ -39,9 +39,9 @@ app.use('/', require('./routes/index'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 /**
@@ -49,22 +49,22 @@ app.use(function(req, res, next) {
  */
 // development error handler will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('Error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('Error', {
+        message: err.message,
+        error: err
+        });
     });
-  });
 }
 
 // production error handler will not leak stacktrace
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('Error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('Error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 /**
--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ var router = express.Router();
  * GET index page.
  */
 router.get('/', function(req, res, next) {
-  res.render('Index', { title: 'Express + React-Engine' });
+    res.render('Index', { title: 'Express + React-Engine' });
 });
 
 /**
