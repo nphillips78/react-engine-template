63ad0bee61fe7627d4494dcbd67916e3af6742db
Mark <mark@remarkablemark.org>
2016-09-13 00:47:31 -0400

    Get and parse the versions config in `main.js`

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,16 @@ require('../css/style.css');
 (function(window, document){
     'use strict';
 
+    // config
+    try {
+        var config = JSON.parse(
+            document.getElementById('data-config').getAttribute('data-config')
+        );
+        var versions = config.versions;
+    } catch (error) {
+        // console.log(error);
+    }
+
     /**
      * Client-side mounting.
      */
