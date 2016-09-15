972ea5a00ceb6814072d988d3a2154981c2670e4
Mark <mark@remarkablemark.org>
2016-09-15 14:08:32 -0400

    Update babel-loader to transpile files with `.js` extension
    Currently the `babel-loader` only transpiles files with `.jsx`
    extension.
    
    This allows client-side `.js` files to use es2015 features:
    - https://babeljs.io/docs/plugins/preset-es2015/

--- webpack/production.config.js
+++ webpack/production.config.js
@@ -26,7 +26,7 @@ module.exports = {
     module: {
         loaders: [
             {
-                test: /\.jsx$/,
+                test: /\.jsx?$/,
                 exclude: /(node_modules|bower_components)/,
                 loader: 'babel-loader',
                 query: {
